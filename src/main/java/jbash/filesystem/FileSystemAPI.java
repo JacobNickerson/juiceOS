64e2527ca3091a4f882c1e09a104889e18dd79d1
Jacob Nickerson <jacobnickerson817@gmail.com>
2024-09-22 10:16:11 -0400

    Added new methods involving paths, fixed issues with findFSO
    Added method overloading to the methods for creating files
    and directories. Now, files and directories can be created by path.
    This should be easier and more useful than having to find a reference
    first. Also added a new method for changing the current directory,
    eventually this should be moved to be a shell member, but for now
    this works.

--- src/main/java/jbash/filesystem/FileSystemAPI.java
+++ src/main/java/jbash/filesystem/FileSystemAPI.java
@@ -13,8 +13,13 @@ public class FileSystemAPI {
         this.currentDirectory = this.root;
     }
 
+    // TODO: Add support for quotes
     public Optional<FileSystemObject> getFileSystemObject(String path) {
+        if (path.isEmpty()) { return Optional.empty(); }
+        boolean directorySearch = false;
+        if (path.endsWith("/")) { directorySearch = true; }  // Paths ending in / can only reference directories
         List<String> pathArgs = Arrays.stream(path.split("/")).filter(s -> !s.isEmpty()).toList();
+        if (pathArgs.isEmpty()) { return Optional.of(root); }
         Directory workingDirectory;
         if (pathArgs.getFirst().equals(".")) {  // starting in current directory
             workingDirectory = this.currentDirectory;
@@ -38,7 +43,16 @@ public class FileSystemAPI {
                         }
                 }
             } else {
-                return workingDirectory.findChild(pathArgs.get(i));
+                switch(pathArgs.get(i)) {
+                    case ".":
+                        return Optional.of(workingDirectory);
+                    case "..":
+                        return Optional.of(workingDirectory.getParent());
+                    default:
+                        Optional<FileSystemObject> foundObject = workingDirectory.findChild(pathArgs.get(i));
+                        if (directorySearch && (foundObject.isEmpty() || !(foundObject.get() instanceof Directory))) { return Optional.empty(); }
+                        return workingDirectory.findChild(pathArgs.get(i));
+                }
             }
         }
         return Optional.empty();
@@ -58,6 +72,16 @@ public class FileSystemAPI {
 
     public boolean createFile(String name, Directory parent) {
         if (parent == null || name.isEmpty()) { return false; }
+        if (parent.findChild(name).isPresent()) { return false; }
+        File newFile = new File(name, parent);
+        parent.addChild(newFile);
+        return true;
+    }
+
+    public boolean createFile(String name, String parentPath) {
+        Optional<FileSystemObject> parentOptional = getFileSystemObject(parentPath);
+        if (parentOptional.isEmpty() || !(parentOptional.get() instanceof Directory parent) || name.isEmpty()) { return false; }
+        if (parent.findChild(name).isPresent()) { return false; }
         File newFile = new File(name, parent);
         parent.addChild(newFile);
         return true;
@@ -65,6 +89,16 @@ public class FileSystemAPI {
 
     public boolean createFile(String name, Directory parent, String contents) {
         if (parent == null || name.isEmpty()) { return false; }
+        if (parent.findChild(name).isPresent()) { return false; }
+        File newFile = new File(name, parent, contents);
+        parent.addChild(newFile);
+        return true;
+    }
+
+    public boolean createFile(String name, String parentPath, String contents) {
+        Optional<FileSystemObject> parentOptional = getFileSystemObject(parentPath);
+        if (parentOptional.isEmpty() || !(parentOptional.get() instanceof Directory parent) || name.isEmpty()) { return false; }
+        if (parent.findChild(name).isPresent()) { return false; }
         File newFile = new File(name, parent, contents);
         parent.addChild(newFile);
         return true;
@@ -72,6 +106,16 @@ public class FileSystemAPI {
 
     public boolean createDirectory(String name, Directory parent) {
         if (parent == null || name.isEmpty()) { return false; }
+        if (parent.findChild(name).isPresent()) { return false; }
+        Directory newDirectory = new Directory(name, parent);
+        parent.addChild(newDirectory);
+        return true;
+    }
+
+    public boolean createDirectory(String name, String parentPath) {
+        Optional<FileSystemObject> parentOptional = getFileSystemObject(parentPath);
+        if (parentOptional.isEmpty() || !(parentOptional.get() instanceof Directory parent) || name.isEmpty()) { return false; }
+        if (parent.findChild(name).isPresent()) { return false; }
         Directory newDirectory = new Directory(name, parent);
         parent.addChild(newDirectory);
         return true;
@@ -86,6 +130,13 @@ public class FileSystemAPI {
         newParent.addChild(FSO);
         FSO.setParent(newParent);
         FSO.updatePath();
+    }
+
+    public boolean moveCurrentDirectory(String path) {
+        Optional<FileSystemObject> newDirectoryOptional = getFileSystemObject(path);
+        if (newDirectoryOptional.isEmpty() || !(newDirectoryOptional.get() instanceof Directory)) { return false; }
+        this.currentDirectory = (Directory) newDirectoryOptional.get();
+        return true;
     }
 
 }
