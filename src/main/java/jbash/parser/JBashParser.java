b0e6d7c4244d44cec85f98355f60d73567b97f6f
unknown <christopherbloodsworth@gmail.com>
2024-09-27 14:50:07 -0400

    Remove some fat from the parser class.

--- src/main/java/jbash/parser/JBashParser.java
+++ src/main/java/jbash/parser/JBashParser.java
@@ -1,11 +1,6 @@
 package jbash.parser;
 
-import jbash.jbashutils.JBashUtils;
-
 import java.util.*;
-import java.util.stream.Collectors;
-
-import static jbash.jbashutils.JBashUtils.findFirstOf;
 
 public final class JBashParser {
     private final ArrayList<Token> tokens = new ArrayList<>();
@@ -13,29 +8,10 @@ public final class JBashParser {
         this.tokens.addAll(tokens);
     }
 
-    /**
-     * Processes redirection operators on the internal token list.
-     */
-    private void processRedirects() {
-        if (tokens.isEmpty()) throw new RuntimeException("processRedirects() called on empty token list.");
-
-//        for (int i = 0; i < tokens.size(); i++) {
-//            var t = tokens.get(i);
-//            if (t.type() = TokenType.Great) {
-//                var next = tokens.get(++i);
-//                while (next.type() != TokenType.Whitespace) {
-//
-//                }
-//            }
-//        }
-    }
 
     public static ArrayList<String> parseCommand(String input) throws JBParserException {
         if (input.isEmpty()) { return new ArrayList<>(); }
         var parser = new JBashParser(JBashLexer.lexCommand(input));
-
-        // Parsing phase
-        parser.processRedirects();
 
         return new ArrayList<>(
                 List.of(parser.tokens.parallelStream()
@@ -43,5 +19,4 @@ public final class JBashParser {
                         .reduce("", (s, t) -> s + t)
                         .split("\0")));
     }
-
 }
