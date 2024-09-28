4d66e4ce5142c361d1c89bfd0e2385644c970bd3
Jacob Nickerson <jacobnickerson817@gmail.com>
2024-09-28 18:47:19 -0400

    Adds new command method err for standard error messages, fixes ls
    This commit adds a new method `err`, that wraps CmdErr and can be
    used to print standardized error message for each command. Additionally,
    ls was broken in the previous pr, one instance of System.out.write
    was missed and not replaced with the new CmdPrint.

--- src/main/java/jbash/commands/CmdMkdir.java
+++ src/main/java/jbash/commands/CmdMkdir.java
@@ -17,13 +17,13 @@ class CmdMkdir extends Command {
     @Override
     public int execute(List<String> argv) {
         if (argv.isEmpty()) {
-            cmdErrln("mkdir: missing operand");
+            err("missing operand");
             cmdErrln("Try 'mkdir --help' for more information." );
         }
         FileSystemAPI FSAPI = FileSystemAPI.getInstance();
         for (String arg : argv) {
             if (!FSAPI.createDirectory(arg)) {
-                cmdErrln("mkdir: cannot create directory '" + arg + "'");
+                err("cannot create directory '" + arg +"'");
             }
         }
         return 0;
